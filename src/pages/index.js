c35ff3d551a69984be1a899d7a15614fcdd463d0
Victor Velasquez <victorv24@gmail.com>
2019-06-03 21:22:07 -0700

    A quick update to the main page text.

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Public Space Travel</h1>
+    <p>A podcast.</p>
+
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
