42f9391c39eb9623989e05bc42a814aecb9a95fa
Victor Velasquez <victorv24@gmail.com>
2019-06-04 19:57:53 -0700

    Added basic copy and installed Lazy Hero package.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,67 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Public Space Travel</h1>
-    <p>A podcast.</p>
 
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h2>What The—?</h2>
+    <p>
+      Public Space Travel is a podcast dedicated to intersectional
+      social/political critique, cultural studies, comedy, and education.
+    </p>
+    <p>
+      It originated from the ideas that: Knowledge ought to be more public as
+      opposed to inaccessible due to academic paywalls and lack of resources;
+      That democracy is an ongoing and necessary process for liberation; and
+      that social justice requires an intersectional, decolonizing and
+      anti-capitalist approach.{" "}
+    </p>
+    <p>We strive for that P R A X I S (Theory and Action).</p>
+    <p>
+      We talk about current events. Sometimes we play a continuing narrative
+      RPG. Sometimes we drink too much coffee and talk about random interesting
+      shit.
+    </p>
+    <p>
+      But we always come come from an anti-hierarchy/oppression perspective, we
+      aim for progressive/radical left solidarity with brothers/sisters/trans
+      and non-binary folk of all stripes.
+    </p>
+
+    <h2>Who the—?</h2>
+    <p>
+      Lazarus Wolf (@PSTLazarus): A PhD student and Anarchx-adjunct professor,
+      trained in philosophy but agrees that "Philosophers have hitherto only
+      interpreted the world in various ways; the point is to change it."
+    </p>
+    <p>
+      Luci (@PSTLuci): An anarchist philosophically and a communist politically.
+      Voted most likely to watch the world burn. Has a background in music and
+      web development. Lurker 4 lyfe.
+    </p>
+    <p>
+      Mar(x) aka Shadow Producer (@PSTinTheShadows): A student of the 1s and 2s;
+      a scholar of the wheels of steel. Combining leftist social and political
+      ideology and an interest in audio engineering to produce this pod.{" "}
+    </p>
+    <p>
+      Hunty (@PSTHunty): An advocate for social justice, equity, and revolution
+      as necessary.
+    </p>
+
+    <h2>How the—?</h2>
+    <p>
+      Listen on major podcast/music sites e.g. itunes, spotify, or here:
+      https://anchor.fm/public-space-travel{" "}
+    </p>
+    <p>Leave us Voicemails: (208) 502-1406</p>
+    <p>Write us Emails: PublicSpaceTravel@gmail.com</p>
   </Layout>
 )
 
