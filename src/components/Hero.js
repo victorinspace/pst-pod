0e2ac28ec8f26319ca48c0ede22a6b9eadc64a19
Victor Velasquez <victorv24@gmail.com>
2019-06-13 16:01:59 -0700

    Increased hero and hero font size. Changed text color. Added new photo background. Added black overlay behind the text.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,8 +3,14 @@ import LazyHero from "react-lazy-hero"
 
 const Hero = () => (
   <div>
-    <LazyHero imageSrc="https://unsplash.it/2000/1000">
-      <h1 style={{ fontSize: `4em` }}>Public Space Travel</h1>
+    <LazyHero
+      imageSrc="https://images.unsplash.com/photo-1485304018316-1ff471aa1369?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+      color="#000"
+      opacity="0.2"
+      parallaxOffset="100"
+      minHeight="75vh"
+    >
+      <h1 style={{ fontSize: `9em`, color: `#eee` }}>Public Space Travel</h1>
     </LazyHero>
   </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,106 +7,108 @@ import SEO from "../components/seo"
 import Hero from "../components/Hero"
 
 const IndexPage = () => (
-  <Layout>
+  <div>
     <Hero />
-    <SEO title="Home" />
+    <Layout>
+      <SEO title="Home" />
 
-    <h2 style={{ paddingTop: `2rem` }}>What The—?</h2>
-    <p>
-      Public Space Travel is a podcast dedicated to intersectional
-      social/political critique, cultural studies, comedy, and education.
-    </p>
-    <p>
-      It originated from the ideas that: Knowledge ought to be more public as
-      opposed to inaccessible due to academic paywalls and lack of resources;
-      That democracy is an ongoing and necessary process for liberation; and
-      that social justice requires an intersectional, decolonizing and
-      anti-capitalist approach.{" "}
-    </p>
-    <p>We strive for that P R A X I S (Theory and Action).</p>
-    <p>
-      We talk about current events. Sometimes we play a continuing narrative
-      RPG. Sometimes we drink too much coffee and talk about random interesting
-      shit.
-    </p>
-    <p>
-      But we always come come from an anti-hierarchy/oppression perspective, we
-      aim for progressive/radical left solidarity with brothers/sisters/trans
-      and non-binary folk of all stripes.
-    </p>
+      <h2 style={{ paddingTop: `2rem` }}>What The—?</h2>
+      <p>
+        Public Space Travel is a podcast dedicated to intersectional
+        social/political critique, cultural studies, comedy, and education.
+      </p>
+      <p>
+        It originated from the ideas that: Knowledge ought to be more public as
+        opposed to inaccessible due to academic paywalls and lack of resources;
+        That democracy is an ongoing and necessary process for liberation; and
+        that social justice requires an intersectional, decolonizing and
+        anti-capitalist approach.{" "}
+      </p>
+      <p>We strive for that P R A X I S (Theory and Action).</p>
+      <p>
+        We talk about current events. Sometimes we play a continuing narrative
+        RPG. Sometimes we drink too much coffee and talk about random
+        interesting shit.
+      </p>
+      <p>
+        But we always come come from an anti-hierarchy/oppression perspective,
+        we aim for progressive/radical left solidarity with
+        brothers/sisters/trans and non-binary folk of all stripes.
+      </p>
 
-    <h2 style={{ paddingTop: `2rem` }}>Who the—?</h2>
-    <p>
-      Lazarus Wolf (
-      <a
-        href="https://twitter.com/PSTLazarus"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        @PSTLazarus
-      </a>
-      ): A PhD student and Anarchx-adjunct professor, trained in philosophy but
-      agrees that "Philosophers have hitherto only interpreted the world in
-      various ways; the point is to change it."
-    </p>
-    <p>
-      Luci (
-      <a
-        href="https://twitter.com/PSTLuci"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        @PSTLuci
-      </a>
-      ): An anarchist philosophically and a communist politically. Voted most
-      likely to watch the world burn. Has a background in music and web
-      development. Lurker 4 lyfe.
-    </p>
-    <p>
-      Mar(x) aka Shadow Producer (
-      <a
-        href="https://twitter.com/PSTinTheShadows"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        @PSTinTheShadows
-      </a>
-      ): A student of the 1s and 2s; a scholar of the wheels of steel. Combining
-      leftist social and apolitical ideology and an interest in audio
-      engineering to produce this pod.{" "}
-    </p>
-    <p>
-      Hunty (
-      <a
-        href="https://twitter.com/PSTHunty"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        @PSTHunty
-      </a>
-      ): An advocate for social justice, equity, and revolution as necessary.
-    </p>
+      <h2 style={{ paddingTop: `2rem` }}>Who the—?</h2>
+      <p>
+        Lazarus Wolf (
+        <a
+          href="https://twitter.com/PSTLazarus"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          @PSTLazarus
+        </a>
+        ): A PhD student and Anarchx-adjunct professor, trained in philosophy
+        but agrees that "Philosophers have hitherto only interpreted the world
+        in various ways; the point is to change it."
+      </p>
+      <p>
+        Luci (
+        <a
+          href="https://twitter.com/PSTLuci"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          @PSTLuci
+        </a>
+        ): An anarchist philosophically and a communist politically. Voted most
+        likely to watch the world burn. Has a background in music and web
+        development. Lurker 4 lyfe.
+      </p>
+      <p>
+        Mar(x) aka Shadow Producer (
+        <a
+          href="https://twitter.com/PSTinTheShadows"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          @PSTinTheShadows
+        </a>
+        ): A student of the 1s and 2s; a scholar of the wheels of steel.
+        Combining leftist social and apolitical ideology and an interest in
+        audio engineering to produce this pod.{" "}
+      </p>
+      <p>
+        Hunty (
+        <a
+          href="https://twitter.com/PSTHunty"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          @PSTHunty
+        </a>
+        ): An advocate for social justice, equity, and revolution as necessary.
+      </p>
 
-    <h2 style={{ paddingTop: `2rem` }}>How the—?</h2>
-    <p>
-      Listen on major podcast/music sites e.g. itunes, spotify, or{" "}
-      <a
-        href="https://anchor.fm/public-space-travel"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        here
-      </a>
-      .
-    </p>
-    <p>
-      Write to us:
-      <a href="PublicSpaceTravel@gmail.com">PublicSpaceTravel@gmail.com</a>
-    </p>
-    <p>
-      Leave us a voicemail: <a href="tel:+12085021406">(208) 502-1406</a>
-    </p>
-  </Layout>
+      <h2 style={{ paddingTop: `2rem` }}>How the—?</h2>
+      <p>
+        Listen on major podcast/music sites e.g. itunes, spotify, or{" "}
+        <a
+          href="https://anchor.fm/public-space-travel"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          here
+        </a>
+        .
+      </p>
+      <p>
+        Write to us:
+        <a href="PublicSpaceTravel@gmail.com">PublicSpaceTravel@gmail.com</a>
+      </p>
+      <p>
+        Leave us a voicemail: <a href="tel:+12085021406">(208) 502-1406</a>
+      </p>
+    </Layout>
+  </div>
 )
 
 export default IndexPage
